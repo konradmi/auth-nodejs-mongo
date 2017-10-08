87fa7642fbe351d7e24b6f1034dc0fc15d0a9c48
KonradM <miekus.konrad@gmail.com>
2017-10-08 21:34:02 +0200

    Add cors and change the format of the example authenticated message

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const router = require('./router')
 
@@ -10,6 +11,7 @@ const app = express()
 
 mongoose.connect('mongodb://localhost:auth/auth')
 
+app.use(cors())
 app.use(morgan('combined'))
 app.use(bodyParser.json({ type: '*/*' }))
 router(app)
--- router.js
+++ router.js
@@ -6,9 +6,7 @@ const requireAuth = passport.authenticate('jwt', { session : false })
 const requireSignin = passport.authenticate('local', { session : false })
 
 module.exports = (app) => {
-  app.get('/', requireAuth, (req, res) => {
-  	res.send({hi: 'there'})
-  })
+  app.get('/', requireAuth, (req, res) => res.send({message: 'hi there'}))
   
   app.post('/signup', Authentication.signup)
   app.post('/signin', requireSignin, Authentication.signin)
